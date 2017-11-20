24cfeaab063c41b42133c072992b9a9b1b107974
Alexander Shishenko <alex@shishenko.com>
2017-11-21 00:42:18 +0300

    http-parser library

--- conanfile.py
+++ conanfile.py
@@ -5,43 +5,41 @@ from conans import ConanFile, CMake, tools
 import os
 
 
-class LibnameConan(ConanFile):
-    name = "libname"
-    version = "0.0.0"
-    url = "https://github.com/bincrafters/conan-libname"
+class HttpParserConan(ConanFile):
+    name = "http_parser"
+    version = "2.7.1"
+    year = "2017"
+    url = "https://github.com/bincrafters/conan-http-parser"
     description = "Keep it short"
-    license = "https://github.com/someauthor/somelib/blob/master/LICENSES"
+    license = "https://github.com/nodejs/http-parser/blob/master/LICENSE-MIT"
     settings = "os", "arch", "compiler", "build_type"
-    options = {"shared": [True, False]}
-    default_options = "shared=False"
-    #use static org/channel for libs in conan-center
-    #use dynamic org/channel for libs in bincrafters
-    requires = "OpenSSL/1.0.2l@conan/stable", \
-        "zlib/1.2.11@conan/stable", \
-        "websocketpp/0.7.0@%s/%s" % (self.user, self.channel)
+    generators = "cmake"
+    exports = ["CMakeLists.txt"]
+    options = {"shared": [True, False], "strict": [True, False]}
+    default_options = "shared=False", "strict=False"
 
     def source(self):
-        source_url = "https://github.com/Microsoft/cpprestsdk"
+        source_url = "https://github.com/nodejs/http-parser"
         tools.get("{0}/archive/v{1}.tar.gz".format(source_url, self.version))
-        extracted_dir = self.name + "-" + self.version
+        extracted_dir = self.name.replace("_", "-") + "-" + self.version
         os.rename(extracted_dir, "sources")
-        #Rename to "sources" is a convention to simplify later steps
 
     def build(self):
         cmake = CMake(self)
-        cmake.definitions["BUILD_TESTS"] = False # example
-        cmake.configure(source_dir="sources")
+        cmake.definitions["HTTP_PARSER_STRICT"] = self.options.strict
+        cmake.configure()
         cmake.build()
 
     def package(self):
         with tools.chdir("sources"):
             self.copy(pattern="LICENSE")
-            self.copy(pattern="*", dst="include", src="include")
+            self.copy(pattern="*", dst="include", src="sources")
             self.copy(pattern="*.dll", dst="bin", src="bin", keep_path=False)
             self.copy(pattern="*.lib", dst="lib", src="lib", keep_path=False)
             self.copy(pattern="*.a", dst="lib", src="lib", keep_path=False)
             self.copy(pattern="*.so*", dst="lib", src="lib", keep_path=False)
             self.copy(pattern="*.dylib", dst="lib", src="lib", keep_path=False)
+            self.copy(pattern="*.pdb", dst="lib", keep_path=False)
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
